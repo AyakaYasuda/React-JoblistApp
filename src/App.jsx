e72e3717e5b7e43b28400667483dabc5377b5d45
Ayaka Yasuda <ayakayasuda23@gmail.com>
2022-01-05 14:03:53 -0800

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { JobList } from './components/Jobs/JobList';
+
+
+
+const App = () => {
+    return (
+        <div>
+            <JobList />
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/Jobs/JobList.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const JobList = () => {};
+
+export default JobList;
